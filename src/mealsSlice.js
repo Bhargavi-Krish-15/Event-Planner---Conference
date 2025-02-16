4ff411e766980b8dbf29fcfc4dae3dd306c7553d
Bhargavi Krishnamurthi <bhargavikrish@mac.mynetworksettings.com>
2025-02-16 16:30:13 -0500

    project fully done

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,17 +1,22 @@
 // mealsSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  { name: "Breakfast", cost: 50, selected: false },
+  { name: "High Tea", cost: 25, selected: false },
+  { name: "Lunch", cost: 65, selected: false },
+  { name: "Dinner", cost: 70, selected: false },
+];
 
 export const mealsSlice = createSlice({
-  name: 'meals',
-  initialState: [
-   
-  ],
+  name: "meals",
+  initialState,
   reducers: {
     toggleMealSelection: (state, action) => {
+      state[action.payload].selected = !state[action.payload].selected;
     },
   },
 });
 
 export const { toggleMealSelection } = mealsSlice.actions;
-
 export default mealsSlice.reducer;
